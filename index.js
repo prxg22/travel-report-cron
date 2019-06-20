46abb54a56aca3f27a0ffcf68f7d4e5b94d695a8
Paulo Ricardo Xavier Giusti <prxg22@gmail.com>
2019-06-20 14:43:07 -0300

    fix: firebase auth

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const tr = require('travel-report')
 const trc = require('travel-report-crawler')
 const admin = require('firebase-admin')
+const dotenv = require('dotenv').config()
 const { Travel, Report } = tr(admin, { ref: '/chats/{chat_id}', params: { chat_id: '' }})
 
 const config = {
   apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
 }
 
 admin.initializeApp(config)
